146a8fcf29b9f56ff488c3bb78a399aecb879a1e
Lucas Peinado Bruscato <lucas.bruscato@gmail.com>
2019-02-23 21:07:28 +0100

    update: name for each exercise

--- helper.py
+++ helper.py
@@ -3,16 +3,30 @@ import os
 import time
 
 ######### only change here #########
-timeInSeconds = 60
-numberOfExercises = 13
+timeInSeconds = 40
 ####################################
 
-os.system('say "começou"')
+exercises = ['joga para cima',
+            'alonga o tronco para a direita',
+            'alonga o tronco para a esquerda',
+            'puxa o braço para direita e olha para esquerda',
+            'puxa o braço para esquerda e olha para direita',
+            'alonga o punho direito para baixo',
+            'alonga o punho esquerdo para baixo',
+            'alonga o punho direito para cima',
+            'alonga o punho esquerdo para cima',
+            'alonga perna direita',
+            'alonga perna esquerda',
+            'alonga tronco para baixo',
+            'sentado, alonga tronco para frente',
+            'posiçao cleópatra para direita',
+            'posiçao cleópatra para esquerda',
+            'alonga igual cachorro de férias',
+            'bora aproveitar o dia']
+
+os.system('say ' + exercises[0])
 time.sleep(5)
 
-for i in range(0, numberOfExercises):
+for i in range(1, len(exercises)):
     time.sleep(timeInSeconds)
-    if i != numberOfExercises - 1:
-        os.system('say "próximo"')
-
-os.system('say "fim"')
+    os.system('say ' + exercises[i])
