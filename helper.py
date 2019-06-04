5cf2df2010fd6f44a4609dba2ca565f3596b7185
Lucas Peinado Bruscato <lucas.bruscato@gmail.com>
2019-06-04 17:46:18 +0200

    update

--- helper.py
+++ helper.py
@@ -4,33 +4,34 @@ import time
 import datetime
 
 ######### change here #########
-timeInSeconds = 40
+time_in_seconds = 34
 ###############################
 
 exercises = ['joga para cima',
-            'alonga o tronco para a direita',
-            'alonga o tronco para a esquerda',
-            'puxa o braço para direita e olha para esquerda',
-            'puxa o braço para esquerda e olha para direita',
-            'alonga o punho direito para baixo',
-            'alonga o punho esquerdo para baixo',
-            'alonga o punho direito para cima',
-            'alonga o punho esquerdo para cima',
-            'alonga perna direita',
-            'alonga perna esquerda',
-            'alonga tronco para baixo',
-            'sentado, alonga tronco para frente',
-            'posiçao cleópatra para direita',
-            'posiçao cleópatra para esquerda',
-            'alonga igual cachorro de férias',
-            'bora aproveitar o dia']
+             'alonga o tronco para baixo',
+             'alonga o tronco para a direita',
+             'alonga o tronco para a esquerda',
+             'alonga o punho direito para baixo',
+             'alonga o punho esquerdo para baixo',
+             'alonga o punho direito para cima',
+             'alonga o punho esquerdo para cima',
+             'alonga a cabeça para a direita',
+             'alonga a cabeça para a esquerda',
+             'gira a cabeça lentamente',
+             'alonga a perna direita',
+             'alonga a perna esquerda',
+             'bora aproveitar o dia']
 
 os.system('say bom dia, agora são ' + str(datetime.datetime.now().hour) + ' e ' + str(datetime.datetime.now().minute))
 os.system('say ' + exercises[0])
-time.sleep(5)
+time.sleep(3)
 
 for i in range(1, len(exercises)):
-    time.sleep(timeInSeconds)
+
+    for j in range(1, time_in_seconds):
+        print(exercises[i-1] + ' - ' + str(j))
+        time.sleep(1)
     os.system('say ' + exercises[i])
+
 
 os.system('say agora são ' + str(datetime.datetime.now().hour) + ' e ' + str(datetime.datetime.now().minute))
